df0a16f7227aa06a55cea9340f9fa6853e40a39f
Hugoacr <hugoacr@yahoo.com.br>
2022-07-13 11:46:12 -0300

    fix: refatora model do user

--- app/backend/src/database/models/userModel.ts
+++ app/backend/src/database/models/userModel.ts
@@ -1,12 +1,21 @@
-import { Model, STRING } from 'sequelize';
+import { Model, STRING, INTEGER } from 'sequelize';
 import db from '.';
 
 
 class user extends Model {
-  // public <campo>!: <tipo>;
+public  id?: number;
+public  username: string;
+public  role: string;
+public  email: string;
+public  password: string;
 }
 
 user.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   username: STRING,
   role: STRING,
   email: STRING,
