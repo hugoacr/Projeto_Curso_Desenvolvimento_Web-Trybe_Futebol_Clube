987fcecdde0f19ce5425d2d978a5c22351d1969b
Hugoacr <hugoacr@yahoo.com.br>
2022-07-13 11:44:16 -0300

    fix: refatora arquivo da migration do teams

---
+++ app/backend/src/database/migrations/20220713142318-create-teams.js
@@ -0,0 +1,17 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      team_name: {
+        type: Sequelize.STRING
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('teams');
+  }
+};
