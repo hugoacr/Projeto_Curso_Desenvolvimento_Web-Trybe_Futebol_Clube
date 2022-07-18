97353813df192ff7c4f89feccce9609f0da76828
Hugoacr <hugoacr@yahoo.com.br>
2022-07-18 16:07:51 -0300

    feat: cria route para o User

---
+++ app/backend/src/routes/userRouter.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import UserController from '../controller/userController';
+import UserRepository from '../repositories/userRepository';
+import UserService from '../services/userService';
+import validateLogin from '../middlewares/validateLogin';
+
+const userRouter = Router();
+
+const userFactory = () => {
+    const model = new UserRepository();
+    const service = new UserService(model);
+    const controller = new UserController(service);
+  
+    return controller;
+  };
+
+  userRouter.post('/', validateLogin, (req, res, next) =>{
+    userFactory().UserLogin(req, res, next);
+  });
+
+export default userRouter;
