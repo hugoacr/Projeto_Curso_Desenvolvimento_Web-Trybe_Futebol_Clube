878451cce2475c6fceae36b5e36bbaeb76dc781f
Hugoacr <hugoacr@yahoo.com.br>
2022-07-18 16:03:56 -0300

    feat: cria interfaces para o User

---
+++ app/backend/src/interfaces/userInterface.ts
@@ -0,0 +1,17 @@
+import UserModel from "../database/models/userModel";
+
+export interface IUser {
+  id?: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+export interface IUserRepository {
+  userLogin(email: string, password: string): Promise<UserModel>
+}
+
+export interface IUserService {
+  userLogin(email: string, password: string): Promise<UserModel>
+}
