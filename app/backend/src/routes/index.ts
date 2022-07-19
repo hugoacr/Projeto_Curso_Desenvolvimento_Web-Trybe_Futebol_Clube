742e1d009e07352c11df0760305e40f03f4444b2
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 10:39:14 -0300

    fix: implementa router do EndPoint Get /teams

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import teamsRouter from './teamsRouter';
 import userRouter from './userRouter';
 
 const router = Router();
 
 router.use('/login', userRouter);
+router.use('/teams', teamsRouter);
 
 export default router;
