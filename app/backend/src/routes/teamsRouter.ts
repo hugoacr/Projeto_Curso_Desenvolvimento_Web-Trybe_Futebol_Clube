f9eaa204d10f8535af7d094e41b604cd5f38be86
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 10:37:42 -0300

    feat: cria route para o Teams

---
+++ app/backend/src/routes/teamsRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import TeamsController from '../controller/teamsController';
+import TeamsRepository from '../repositories/teamsRepository';
+import TeamsService from '../services/teamsService';
+
+const teamsRouter = Router();
+
+const teamsFactory = () => {
+    const model = new TeamsRepository();
+    const service = new TeamsService(model);
+    const controller = new TeamsController(service);
+  
+    return controller;
+  };
+
+  teamsRouter.get('/', (req, res, next) =>{
+    teamsFactory().ListTeams(req, res, next);
+  });
+
+export default teamsRouter;
