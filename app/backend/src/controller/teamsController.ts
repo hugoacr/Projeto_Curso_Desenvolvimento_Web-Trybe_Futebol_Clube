fb77817a4df11bbaa3348257117ea2129c120fa7
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 10:37:20 -0300

    feat: cria o controller para o Teams

---
+++ app/backend/src/controller/teamsController.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { ITeamsService } from "../interfaces/teamsInterface";
+
+class TeamsController {
+  constructor(private service: ITeamsService) {
+    this.service = service;
+  }
+  
+  async ListTeams(req: Request, res: Response, next: NextFunction) {
+    try {
+  
+    const teamsData = await this.service.listTeams();
+  
+    return res.status(200).send( teamsData );
+    } catch (error) {
+    next(error);
+    }
+  }
+}
+  
+export default TeamsController;
