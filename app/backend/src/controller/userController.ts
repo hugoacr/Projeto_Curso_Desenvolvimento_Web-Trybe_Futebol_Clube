b273d13a2ea01450df868f5ba2862045d556bca5
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 10:28:50 -0300

    fix: refatora userController inclui o ValidateToken

--- app/backend/src/controller/userController.ts
+++ app/backend/src/controller/userController.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
 import { IUserService } from "../interfaces/userInterface";
 
 class UserController {
@@ -15,6 +16,16 @@ class UserController {
     return res.status(200).send({ token });
     } catch (error) {
     next(error);
+    }
+  }
+
+  async ValidateToken(req: Request, res: Response, next: NextFunction){
+    try {
+      const token = req.headers.authorization as string;
+      const { data: { role } } = jwt.verify(token, `${process.env.JWT_SECRET}`) as jwt.JwtPayload;
+      return res.status(200).json({ role });
+    } catch (error) {
+      next(error);
     }
   }
 }
