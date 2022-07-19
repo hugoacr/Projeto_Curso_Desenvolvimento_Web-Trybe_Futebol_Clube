f5e54239518767c93dfc607b8d52e19e276a00f7
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 17:57:09 -0300

    feat: cria interfaces para o Matches

---
+++ app/backend/src/interfaces/matchesInterface.ts
@@ -0,0 +1,18 @@
+import MatchesModel from "../database/models/matchesModel";
+
+export interface IMatches {
+  id?: number;
+  home_team: number;
+  home_team_goals: number;
+  away_team: number;
+  away_team_goals: number;
+  in_progress: boolean;
+}
+
+export interface IMatchesRepository {
+  listMatches(): Promise<MatchesModel[]>,
+}
+
+export interface IMatchesService {
+  listMatches(): Promise<MatchesModel[]>
+}
