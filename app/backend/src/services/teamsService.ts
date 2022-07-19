2084949619f37e94decbd2d3727a36a218297f34
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 10:37:04 -0300

    feat: cria o service para o Teams

---
+++ app/backend/src/services/teamsService.ts
@@ -0,0 +1,16 @@
+import TeamsModel from '../database/models/teamsModel';
+import { ITeamsService, ITeamsRepository } from '../interfaces/teamsInterface';
+
+class TeamsService implements ITeamsService {
+  constructor(private model: ITeamsRepository) {
+    this.model = model;
+  }
+
+  public async listTeams(): Promise<TeamsModel> {
+    const teamsData = await this.model.listTeams();
+
+    return teamsData as TeamsModel;
+  }
+}
+
+export default TeamsService;
