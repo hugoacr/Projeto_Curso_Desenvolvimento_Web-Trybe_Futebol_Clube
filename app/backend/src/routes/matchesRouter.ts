cea81991eedca981acde4cb07c7129133887c73b
Hugoacr <hugoacr@yahoo.com.br>
2022-07-19 17:58:30 -0300

    feat: cria router para o Matches

---
+++ app/backend/src/routes/matchesRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import MatchesController from '../controller/matchesController';
+import MatchesRepository from '../repositories/matchesRepository';
+import MatchesService from '../services/matchesService';
+
+const matchesRouter = Router();
+
+const matchesFactory = () => {
+    const model = new MatchesRepository();
+    const service = new MatchesService(model);
+    const controller = new MatchesController(service);
+  
+    return controller;
+  };
+
+  matchesRouter.get('/', (req, res, next) =>{
+    matchesFactory().ListMatches(req, res, next);
+  });
+
+export default matchesRouter;
