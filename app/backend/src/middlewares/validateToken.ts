c4e37c5b6a2b15f710567bd6afe3e3c7cbefead4
Hugoacr <hugoacr@yahoo.com.br>
2022-07-20 18:49:31 -0300

    feat: cria o middleware validateToken

---
+++ app/backend/src/middlewares/validateToken.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import * as jwt from "jsonwebtoken";
+import HandleError from '../helpers/handleError';
+
+
+const validateToken = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.headers.authorization as string;
+      jwt.verify(token, `${process.env.JWT_SECRET}`) as jwt.JwtPayload;
+      
+      next();
+    } catch (error) {
+        throw new HandleError(401, 'Token must be a valid token');;
+    };
+}
+
+export default validateToken
