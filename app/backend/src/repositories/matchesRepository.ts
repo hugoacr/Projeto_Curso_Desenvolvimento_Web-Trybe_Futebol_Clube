9a155f86da349c54f3c4c1037d368859771eb3e0
Hugoacr <hugoacr@yahoo.com.br>
2022-07-20 17:12:04 -0300

    fix: inclui validador de times para não gerar matches em times inexistentes no matchesRepository

--- app/backend/src/repositories/matchesRepository.ts
+++ app/backend/src/repositories/matchesRepository.ts
@@ -1,9 +1,14 @@
 import teams from '../database/models/teamsModel';
 import MatchesModel from '../database/models/matchesModel';
+import TeamsModel from '../database/models/teamsModel';
 import { IMatchesRepository } from '../interfaces/matchesInterface';
+import HandleError from '../helpers/handleError';
 
 class MatchesRepository implements IMatchesRepository {
-  constructor(private model = MatchesModel) {}
+  constructor(
+    private model = MatchesModel,
+    private teamModel = TeamsModel
+    ) {}
 
   async listMatches(): Promise<MatchesModel[]> {
     const matchesData = await this.model.findAll({ include: [
@@ -16,6 +21,12 @@ class MatchesRepository implements IMatchesRepository {
 
   async includeMatch(data: Omit<MatchesModel, 'id' | 'inProgress'>): Promise<MatchesModel> {
     const newMatch = await this.model.create(data);
+    const homeTeam = await this.teamModel.findByPk(data.homeTeam);
+    const awayTeam = await this.teamModel.findByPk(data.awayTeam);
+
+    if (!homeTeam || !awayTeam) {
+      throw new HandleError(404, 'There is no team with such id!')
+    }
     
     return newMatch as MatchesModel;
   }
