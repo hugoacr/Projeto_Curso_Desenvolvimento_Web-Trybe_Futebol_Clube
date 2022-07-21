3b6a30ae71294d21293a1762822bccd88f10f083
Hugoacr <hugoacr@yahoo.com.br>
2022-07-21 12:39:05 -0300

    feat: cria repository para leaderboard

---
+++ app/backend/src/repositories/leaderboardsRepository.ts
@@ -0,0 +1,22 @@
+import MatchesModel from '../database/models/matchesModel';
+import TeamsModel from '../database/models/teamsModel';
+import { ILeaderboards, ILeaderboardsRepository } from '../interfaces/leaderboardsInterface';
+import { buildLeaderboard } from '../helpers/functionsLeaderboards'
+
+class LeaderboardsRepository implements ILeaderboardsRepository {
+  constructor(
+    private model = MatchesModel,
+    private teamModel = TeamsModel
+    ) {}
+    
+  async buildLeaderboard(): Promise<ILeaderboards[]> {
+    const matchesData = await this.model.findAll();
+    const teamsData = await this.teamModel.findAll();
+
+    const Leaderboard = buildLeaderboard(teamsData, matchesData)
+    return Leaderboard as ILeaderboards[];
+  }
+
+}
+
+export default LeaderboardsRepository;
