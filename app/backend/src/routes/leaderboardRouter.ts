42e670623957fd0c59affa1fb8b94d11a5705bbe
Hugoacr <hugoacr@yahoo.com.br>
2022-07-21 12:40:30 -0300

    feat: cria rota para o endpoint GET /leaderboard/home

---
+++ app/backend/src/routes/leaderboardRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import LeaderboardController from '../controller/leaderboardController';
+import LeaderboardsRepository from '../repositories/leaderboardsRepository';
+import LeaderboardsService from '../services/leaderboardsService';
+
+const leaderboardRouter = Router();
+
+const leaderboardFactory = () => {
+    const model = new LeaderboardsRepository();
+    const service = new LeaderboardsService(model);
+    const controller = new LeaderboardController(service);
+  
+    return controller;
+  };
+
+  leaderboardRouter.get('/home', (req, res, next) =>{
+    leaderboardFactory().GetLeaderboards(req, res, next);
+  });
+
+export default leaderboardRouter;
