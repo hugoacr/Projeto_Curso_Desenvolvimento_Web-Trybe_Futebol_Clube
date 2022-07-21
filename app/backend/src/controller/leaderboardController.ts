20bd9a27e3176115461a49b7471db9b9ba5534c2
Hugoacr <hugoacr@yahoo.com.br>
2022-07-21 12:39:45 -0300

    feat: cria controller para o leaderboard

---
+++ app/backend/src/controller/leaderboardController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { ILeaderboardsService } from "../interfaces/leaderboardsInterface";
+
+class LeaderboardsController {
+  constructor(private service: ILeaderboardsService) {
+    this.service = service;
+  }
+  
+  async GetLeaderboards(req: Request, res: Response, next: NextFunction) {
+    try {
+    const leaderboardsData = await this.service.getLeaderboards();
+    return res.status(200).send( leaderboardsData );
+    } catch (error) {
+    next(error);
+    }
+  }
+}
+  
+export default LeaderboardsController;
